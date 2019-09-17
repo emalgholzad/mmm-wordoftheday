a1b585427c19df9cacdff0685fbf478d7caf2a18
Emal Gholzad <emalgholzad@gmail.com>
2019-09-17 15:35:15 +0200

    Changed name

--- mmm-wordoftheday.js
+++ mmm-wordoftheday.js
@@ -1,157 +1,156 @@
 /* global Module */
 
 /* Magic Mirror
- * Module: {{MODULE_NAME}}
+ * Module: mmm-wordoftheday
  *
  * By {{AUTHOR_NAME}}
  * {{LICENSE}} Licensed.
  */
 
-Module.register("{{MODULE_NAME}}", {
-	defaults: {
-		updateInterval: 60000,
-		retryDelay: 5000
-	},
+Module.register("mmm-wordoftheday", {
+  defaults: {
+    updateInterval: 60000,
+    retryDelay: 5000
+  },
 
-	requiresVersion: "2.1.0", // Required version of MagicMirror
+  requiresVersion: "2.1.0", // Required version of MagicMirror
 
-	start: function() {
-		var self = this;
-		var dataRequest = null;
-		var dataNotification = null;
+  start: function() {
+    var self = this;
+    var dataRequest = null;
+    var dataNotification = null;
 
-		//Flag for check if module is loaded
-		this.loaded = false;
+    //Flag for check if module is loaded
+    this.loaded = false;
 
-		// Schedule update timer.
-		this.getData();
-		setInterval(function() {
-			self.updateDom();
-		}, this.config.updateInterval);
-	},
+    // Schedule update timer.
+    this.getData();
+    setInterval(function() {
+      self.updateDom();
+    }, this.config.updateInterval);
+  },
 
-	/*
-	 * getData
-	 * function example return data and show it in the module wrapper
-	 * get a URL request
-	 *
-	 */
-	getData: function() {
-		var self = this;
+  /*
+   * getData
+   * function example return data and show it in the module wrapper
+   * get a URL request
+   *
+   */
+  getData: function() {
+    var self = this;
 
-		var urlApi = "https://jsonplaceholder.typicode.com/posts/1";
-		var retry = true;
+    var urlApi = "https://jsonplaceholder.typicode.com/posts/1";
+    var retry = true;
 
-		var dataRequest = new XMLHttpRequest();
-		dataRequest.open("GET", urlApi, true);
-		dataRequest.onreadystatechange = function() {
-			console.log(this.readyState);
-			if (this.readyState === 4) {
-				console.log(this.status);
-				if (this.status === 200) {
-					self.processData(JSON.parse(this.response));
-				} else if (this.status === 401) {
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, this.status);
-					retry = false;
-				} else {
-					Log.error(self.name, "Could not load data.");
-				}
-				if (retry) {
-					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
-				}
-			}
-		};
-		dataRequest.send();
-	},
+    var dataRequest = new XMLHttpRequest();
+    dataRequest.open("GET", urlApi, true);
+    dataRequest.onreadystatechange = function() {
+      console.log(this.readyState);
+      if (this.readyState === 4) {
+        console.log(this.status);
+        if (this.status === 200) {
+          self.processData(JSON.parse(this.response));
+        } else if (this.status === 401) {
+          self.updateDom(self.config.animationSpeed);
+          Log.error(self.name, this.status);
+          retry = false;
+        } else {
+          Log.error(self.name, "Could not load data.");
+        }
+        if (retry) {
+          self.scheduleUpdate(self.loaded ? -1 : self.config.retryDelay);
+        }
+      }
+    };
+    dataRequest.send();
+  },
 
+  /* scheduleUpdate()
+   * Schedule next update.
+   *
+   * argument delay number - Milliseconds before next update.
+   *  If empty, this.config.updateInterval is used.
+   */
+  scheduleUpdate: function(delay) {
+    var nextLoad = this.config.updateInterval;
+    if (typeof delay !== "undefined" && delay >= 0) {
+      nextLoad = delay;
+    }
+    nextLoad = nextLoad;
+    var self = this;
+    setTimeout(function() {
+      self.getData();
+    }, nextLoad);
+  },
 
-	/* scheduleUpdate()
-	 * Schedule next update.
-	 *
-	 * argument delay number - Milliseconds before next update.
-	 *  If empty, this.config.updateInterval is used.
-	 */
-	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		nextLoad = nextLoad ;
-		var self = this;
-		setTimeout(function() {
-			self.getData();
-		}, nextLoad);
-	},
+  getDom: function() {
+    var self = this;
 
-	getDom: function() {
-		var self = this;
+    // create element wrapper for show into the module
+    var wrapper = document.createElement("div");
+    // If this.dataRequest is not empty
+    if (this.dataRequest) {
+      var wrapperDataRequest = document.createElement("div");
+      // check format https://jsonplaceholder.typicode.com/posts/1
+      wrapperDataRequest.innerHTML = this.dataRequest.title;
 
-		// create element wrapper for show into the module
-		var wrapper = document.createElement("div");
-		// If this.dataRequest is not empty
-		if (this.dataRequest) {
-			var wrapperDataRequest = document.createElement("div");
-			// check format https://jsonplaceholder.typicode.com/posts/1
-			wrapperDataRequest.innerHTML = this.dataRequest.title;
+      var labelDataRequest = document.createElement("label");
+      // Use translate function
+      //             this id defined in translations files
+      labelDataRequest.innerHTML = this.translate("TITLE");
 
-			var labelDataRequest = document.createElement("label");
-			// Use translate function
-			//             this id defined in translations files
-			labelDataRequest.innerHTML = this.translate("TITLE");
+      wrapper.appendChild(labelDataRequest);
+      wrapper.appendChild(wrapperDataRequest);
+    }
 
+    // Data from helper
+    if (this.dataNotification) {
+      var wrapperDataNotification = document.createElement("div");
+      // translations  + datanotification
+      wrapperDataNotification.innerHTML =
+        this.translate("UPDATE") + ": " + this.dataNotification.date;
 
-			wrapper.appendChild(labelDataRequest);
-			wrapper.appendChild(wrapperDataRequest);
-		}
+      wrapper.appendChild(wrapperDataNotification);
+    }
+    return wrapper;
+  },
 
-		// Data from helper
-		if (this.dataNotification) {
-			var wrapperDataNotification = document.createElement("div");
-			// translations  + datanotification
-			wrapperDataNotification.innerHTML =  this.translate("UPDATE") + ": " + this.dataNotification.date;
+  getScripts: function() {
+    return [];
+  },
 
-			wrapper.appendChild(wrapperDataNotification);
-		}
-		return wrapper;
-	},
+  getStyles: function() {
+    return ["mmm-wordoftheday.css"];
+  },
 
-	getScripts: function() {
-		return [];
-	},
+  // Load translations files
+  getTranslations: function() {
+    //FIXME: This can be load a one file javascript definition
+    return {
+      en: "translations/en.json",
+      es: "translations/es.json"
+    };
+  },
 
-	getStyles: function () {
-		return [
-			"{{MODULE_NAME}}.css",
-		];
-	},
+  processData: function(data) {
+    var self = this;
+    this.dataRequest = data;
+    if (this.loaded === false) {
+      self.updateDom(self.config.animationSpeed);
+    }
+    this.loaded = true;
 
-	// Load translations files
-	getTranslations: function() {
-		//FIXME: This can be load a one file javascript definition
-		return {
-			en: "translations/en.json",
-			es: "translations/es.json"
-		};
-	},
+    // the data if load
+    // send notification to helper
+    this.sendSocketNotification("mmm-wordoftheday-NOTIFICATION_TEST", data);
+  },
 
-	processData: function(data) {
-		var self = this;
-		this.dataRequest = data;
-		if (this.loaded === false) { self.updateDom(self.config.animationSpeed) ; }
-		this.loaded = true;
-
-		// the data if load
-		// send notification to helper
-		this.sendSocketNotification("{{MODULE_NAME}}-NOTIFICATION_TEST", data);
-	},
-
-	// socketNotificationReceived from helper
-	socketNotificationReceived: function (notification, payload) {
-		if(notification === "{{MODULE_NAME}}-NOTIFICATION_TEST") {
-			// set dataNotification
-			this.dataNotification = payload;
-			this.updateDom();
-		}
-	},
+  // socketNotificationReceived from helper
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "mmm-wordoftheday-NOTIFICATION_TEST") {
+      // set dataNotification
+      this.dataNotification = payload;
+      this.updateDom();
+    }
+  }
 });
